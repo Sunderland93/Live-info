a4ff6c74e6a1933628836e0d887fea716abcd9c1
DimaNikolenko <dima.nikola565@gmail.com>
2022-08-04 19:04:05 +1000

    New pre-release version of Live-info 0.2

---
+++ Live_info_0.2.py
@@ -0,0 +1,99 @@
+import platform
+
+def info ():
+    print("""
+Live info 0.2
+-------------
+Machine: %s 
+Architecture: %s  
+Processor: %s 
+Node: %s  
+System: %s 
+Kernel: %s 
+Python-build: %s 
+Python-compiler: %s
+
+""" % (
+    platform.machine(),
+    platform.architecture(),
+    platform.processor(),
+    platform.node(),
+    platform.system(),
+    platform.release(),
+    platform.python_build(),
+    platform.python_compiler(),
+
+))
+    
+info()
+
+
+def logo ():
+    if platform.system()=="Linux":
+        print ("""
+    __    _                 
+   / /   (_)___  __  ___  __
+  / /   / / __ \/ / / / |/_/
+ / /___/ / / / / /_/ />  <  
+/_____/_/_/ /_/\__,_/_/|_|  
+                            """)
+
+
+    if platform.system()=="Windows":
+       print("""                                         
+          _       _________  
+__      _(_)_ __ |___ /___ \ 
+\ \ /\ / / | '_ \  |_ \ __) |
+ \ V  V /| | | | |___) / __/ 
+  \_/\_/ |_|_| |_|____/_____|
+                             
+                                          """)
+
+
+    if platform.system()=="Unix":
+        print("""
+              _           
+             (_)     
+  _   _ _ __  ___  __
+ | | | | '_ \| \ \/ /
+ | |_| | | | | |>  < 
+  \__,_|_| |_|_/_/\_\
+                     
+                      """)
+
+    
+    if platform.system()=="Mac Os":
+        print("""
+
+    __  ___              ____      
+   /  |/  /___ ______   / __ \_____
+  / /|_/ / __ `/ ___/  / / / / ___/
+ / /  / / /_/ / /__   / /_/ (__  ) 
+/_/  /_/\__,_/\___/   \____/____/  
+                                   
+                                 
+                   """)
+
+logo()
+
+
+
+
+
+
+
+
+    
+ 
+    
+    
+    
+
+
+
+
+    
+
+    
+    
+    
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Live info 0.1
+# Live info 0.2 (pre-release)
 #EN: Live info- a simple program to provide system information.
 #Operating systems supporting Live info 0.1:
 #1.Windows
@@ -6,9 +6,13 @@
 #3.Mac Os
 #4 Unix
 #
-#RU: Live info 0.1- простая программа для определения системной информации.
+#RU: Live info - простая программа для определения системной информации.
 #Операционные системы поддерживающие Live info 0.1:
 #1.Windows
 #2.Linux
 #3.Mac Os
 #4.Unix
+#########################################################################
+#EN:What's new?
+#-> the appearance of logos for each operating system
+
