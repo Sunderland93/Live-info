dc351887d1d2f98f7272cf16210d91f62d25d8e0
DimaNikolenko <dima.nikola565@gmail.com>
2022-08-11 01:45:15 +1000

    New version Live info (Live info 0.2.1)

---
+++ Live_info_0.2.1.py
@@ -0,0 +1,77 @@
+import platform
+import cpuinfo
+
+def info ():
+    print("""
+Live info 0.2.1
+-------------
+Architecture: %s  
+Processor: %s 
+Node: %s  
+System: %s 
+Kernel: %s
+CPU: %s
+Python-build: %s 
+Python-compiler: %s
+
+""" % (
+    platform.machine(),
+    platform.processor(),
+    platform.node(),
+    platform.system(),
+    platform.release(),
+    cpuinfo.get_cpu_info()['brand_raw'],
+    platform.python_build(),
+    platform.python_compiler(),
+
+))
+    
+info()
+
+
+def logo ():
+    if platform.system()=="Linux":
+        print ("""
+    __    _                 
+   / /   (_)___  __  ___  __
+  / /   / / __ \/ / / / |/_/
+ / /___/ / / / / /_/ />  <  
+/_____/_/_/ /_/\__,_/_/|_|  
+                            """)
+
+
+    if platform.system()=="Windows":
+       print("""                                         
+          _       _________  
+__      _(_)_ __ |___ /___ \ 
+\ \ /\ / / | '_ \  |_ \ __) |
+ \ V  V /| | | | |___) / __/ 
+  \_/\_/ |_|_| |_|____/_____|
+                             
+                                          """)
+
+
+    if platform.system()=="Unix":
+        print("""
+              _           
+             (_)     
+  _   _ _ __  ___  __
+ | | | | '_ \| \ \/ /
+ | |_| | | | | |>  < 
+  \__,_|_| |_|_/_/\_\
+                     
+                      """)
+
+    
+    if platform.system()=="Darwin":
+        print("""
+______                    _       
+|  _  \                  (_)      
+| | | |__ _ _ ____      ___ _ __  
+| | | / _` | '__\ \ /\ / / | '_ \ 
+| |/ / (_| | |   \ V  V /| | | | |
+|___/ \__,_|_|    \_/\_/ |_|_| |_|
+                                    
+                   """)
+
+logo()
